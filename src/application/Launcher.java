bc18ae5a64a28248f9e251cb645e8e635154bd05
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-22 16:33:42 -0400

    add launcher.java

---
+++ src/application/Launcher.java
@@ -0,0 +1,9 @@
+package application;
+
+import static javafx.application.Application.launch;
+
+public class Launcher {
+    public static void main(String[] args) {
+        launch(Main.class, args);
+    }
+}
