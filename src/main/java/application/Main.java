b47e5558295327d73763b1874beb13409254f974
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-04-22 16:58:56 -0400

    clean up pom and remove Main.main

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -27,9 +27,4 @@ public class Main extends Application {
         primaryStage.centerOnScreen();
         primaryStage.show();
     }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
